8decfbb904be8b8a4388af964e61da0e9d37ca6b
Tarun-tej-2007 <taruntej947@gmail.com>
2026-02-06 13:31:50 +0530

    firbase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(CounterApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(CounterApp());
+}
 
 class CounterApp extends StatefulWidget {
   @override
